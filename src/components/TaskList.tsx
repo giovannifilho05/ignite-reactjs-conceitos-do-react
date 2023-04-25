f3a1401f1d41b66a553244cebd36a1b07d64600e
Giovanni Filho <giovannifilho0905@hotmail.com>
2023-04-24 23:37:51 -0300

    test2

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -17,11 +17,11 @@ export function TaskList() {
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
     if(newTaskTitle) {
-      setTasks(...tasks, {
+      setTasks([...tasks, {
         id: Math.random(),
         title: newTaskTitle,
         isComplete: false
-      })
+      }])
     }
   }
 
